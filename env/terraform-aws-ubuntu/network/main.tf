1fb2dec477f30498691c92cfbd0a0ac06839a30c
lsw4701 <pro_4701@naver.com>
2022-10-20 18:53:22 +0900

    fd

--- env/terraform-aws-ubuntu/network/main.tf
+++ env/terraform-aws-ubuntu/network/main.tf
@@ -21,8 +21,7 @@ module "vpc" {
 
   tags = local.common_tags
   
-  private_subnet_tags = { "kubernetes.io/role/internal-elb": 1 }
-  public_subnet_tags  = { "kubernetes.io/role/elb": 1 }
+ 
 
 }
 
